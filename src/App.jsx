966e19215d4f75ae28d92956ddd34d53e32456e1
tanvirhasan2000 <tanvirhasanshawon2000@gmail.com>
2026-06-27 20:17:13 +0600

    27 June upate practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import Countries from './Countries/Countries.jsx'
+import { Suspense, useState } from 'react'
+
+const countriesPromise=fetch('https://openapi.programming-hero.com/api/all')
+  .then(res=>res.json());
 function App() {
- 
+  
 
   return (
-   <Countries/>
+    <div>
+
+      <Suspense fallback={"<p> Countries Loading </p>"}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
+      
+    </div>
+   
   )
 }
 
